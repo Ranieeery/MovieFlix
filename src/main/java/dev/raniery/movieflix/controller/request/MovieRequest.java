8f06f8a7ab76f6605db4be4e0c197794ddc10f83
Ranieeery <raniery2003@hotmail.com>
2025-05-06 11:58:16 -0300

    fix: add JsonFormat annotation to releaseDate in MovieRequest and MovieResponse for consistent date formatting

--- src/main/java/dev/raniery/movieflix/controller/request/MovieRequest.java
+++ src/main/java/dev/raniery/movieflix/controller/request/MovieRequest.java
@@ -5,11 +5,16 @@ import lombok.Builder;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Builder
 public record MovieRequest(
     String title,
     String description,
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     LocalDate releaseDate,
+
     Double rating,
     List<Long> categories,
     List<Long> streamings) {
--- src/main/java/dev/raniery/movieflix/controller/response/MovieResponse.java
+++ src/main/java/dev/raniery/movieflix/controller/response/MovieResponse.java
@@ -1,5 +1,6 @@
 package dev.raniery.movieflix.controller.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalDate;
@@ -10,7 +11,10 @@ public record MovieResponse(
     Long id,
     String title,
     String description,
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     LocalDate releaseDate,
+
     Double rating,
     List<CategoryResponse> categories,
     List<StreamingResponse > streamings) {
